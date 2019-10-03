587c01c1c689a7d61ff7e315ef9dc2bc841a12fb
natchanatcha <51436676+natchanatcha@users.noreply.github.com>
2019-10-03 10:51:57 +0700

    Update app.py

--- app.py
+++ app.py
@@ -20,23 +20,18 @@ def callback():
     json_line = request.get_json()
     json_line = json.dumps(json_line)
     decoded = json.loads(json_line)
-    user = decoded['originalDetectIntentRequest']['payload']['data']['replyToken']
-    #user = decoded["events"][0]['replyToken']       
-    userText = decoded['queryResult']['intent']['displayName']
-    #userText = decoded["events"][0]['message']['text']                       
-    #sendText(user,userText)
-    if (userText == 'สวัสดี') :
-        sendText(user,'สวัสดีจ้ะ')
-    elif (userText == 'ไปเที่ยวกันไหม') :
-        sendText(user,'ไม่ไปไม่ว่าง')
+    user = decoded['originalDetectIntentRequest']['payload']['data']['replyToken'] #user = decoded["events"][0]['replyToken']
+    userText = decoded['queryResult']['intent']['displayName']     #userText = decoded["events"][0]['message']['text']                   
+    if (a1 == 'สวัสดี') :
+        print('สวัสดีจ้ะ')
+    elif (a1 == 'สบายดีมั้ย') :
+        print('สบายดี')
     else :
-        sendText(user,'พิมพ์อะไรมาอ่ะ')
-
-    return '',200
-
-def sendText(user, text):
-  LINE_API = 'https://api.line.me/v2/bot/message/reply'
-  headers = {
+        print('อะไรอะ')
+   return '',200
+    def sendText(user, text):
+    LINE_API = 'https://api.line.me/v2/bot/message/reply'
+    headers = {
     'Content-Type': 'application/json; charset=UTF-8',
     'Authorization': os.environ['Authorization']    # ตั้ง Config vars ใน heroku พร้อมค่า Access token
   }
